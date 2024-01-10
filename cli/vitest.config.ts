8b1e13b25d37c15fac3aa7e1f03681f41aa02002
Damien Smrt <dsmrt@users.noreply.github.com>
2024-01-09 17:53:45 -0700

    coverage and publishing (#11)
    * config coverage
    
    * fixing lerna version
    
    * fix publishing
    
    * fix ci

--- cli/vitest.config.ts
+++ cli/vitest.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    globals: true,
     include: ["src/**/*.test.ts"],
     coverage: {
       provider: "v8",
       all: true,
       exclude: ["lib/**/*", "src/__mocks__/**/*"],
+      reporter: ["text-summary", "json-summary"],
     },
   },
 });
---
+++ scripts/coverage.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+
+const PATHS = ["aws-sdk", "cli", "config"];
+
+const BADGE_CONFIG = {};
+
+PATHS.forEach((path) => {
+  const summary = JSON.parse(
+    fs.readFileSync(`${path}/coverage/coverage-summary.json`).toString(),
+  );
+
+  // .total.lines.pct
+  console.log(`${path}:${summary.total.lines.pct}%`);
+});
