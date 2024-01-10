c7137adeecdd5c64ac8ebdd494ec03f1aeff3728
Damien Smrt <dsmrt86@gmail.com>
2024-01-09 17:29:26 -0700

    config coverage

--- aws-sdk/vitest.config.ts
+++ aws-sdk/vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
       provider: "v8",
       all: true,
       exclude: ["lib/**/*", "src/__mocks__/**/*"],
+      reporter: ["text-summary", "json-summary"],
     },
   },
 });
